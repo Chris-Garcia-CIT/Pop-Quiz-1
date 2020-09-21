b485136f14e6954273621b3f27fcd6702f4a9240
Chris-Garcia-CIT <70277532+Chris-Garcia-CIT@users.noreply.github.com>
2020-09-21 11:09:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var QuizEle = document.body.querySelector(".Quiz");
+
+var number=5;
+var othernumber=0;
+var answer=0;
+
+ number=prompt("Give Me Number!");
+if(number<=6){
+  repeat=prompt("Give Me Number!");
+  number++;
+}
+  
+answer=(othernumber+number);
